382f9a4c1ea0d175206587caddf2cac45f2e88d3
lconcteau <lconcteau@sina.com>
2015-04-10 15:46:42 +0800

    Update Test.java

--- src/com/epam/test/Test.java
+++ src/com/epam/test/Test.java
@@ -4,7 +4,11 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("hello");
+		
+		
 		System.out.println("world");
+		
+			System.out.println("world");
 		
 	}
 
